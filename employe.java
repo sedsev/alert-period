e38d4ec33e739da61a6790d0dad87addecbc65f2
leonwekahi <leonnkankamwekahi@yahoo.fr>
2017-03-09 17:11:38 +0100

    ajout employe.java

---
+++ employe.java
@@ -0,0 +1,20 @@
+import java.awt.*;
+import java.io.*;
+import javax.swing.*;
+
+public class employe{
+	protected string matricule;
+	protected int age;
+	protected fonction string;
+	protected int[10] salaire_base
+	protected int[10] salaire_standard
+	
+	public int salaire_employe(string matricule, string fonction){
+		protected taux= 30000
+	 for(i=0; i<salaire_base.length; i++){
+		salaire_base[i]= taux*i/3 + taux;
+			system.out.println(salaire_base[i]);
+										}
+		system.out.println('Voir moi un  feneant; quant il va lire ca il va rire');
+}
+}
